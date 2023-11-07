237d87ef4574c6095c242ee11a10e94505aa8ab2
Serge1sm1rn <serejaz.00@mail.ru>
2023-11-07 21:40:53 +0300

    create POST request for auntification

--- store/users/views.py
+++ store/users/views.py
@@ -1,11 +1,21 @@
+from django.contrib import auth
 from django.shortcuts import render
 
-from users.models import User
 from users.forms import UserLoginForm
 
 
 def login(request):
-    context = {'form': UserLoginForm()}
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
     return render(request, 'users/login.html', context)
 
 
